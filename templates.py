bfe32607900c5e49290f4734139f6c60c5130aab
Shashank Saxena <saxena.shashank96@gmail.com>
2024-04-12 19:39:19 -0400

    Fixing everything

---
+++ templates.py
@@ -0,0 +1,29 @@
+html_table = """
+<html>
+<head>
+  <style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      font-family: Arial, sans-serif;
+    }
+    th, td {
+      padding: 10px;
+      text-align: left;
+      border-bottom: 1px solid #ddd;
+    }
+    th {
+      background-color: #f2f2f2;
+    }
+  </style>
+</head>
+<body>
+  <h1>Contacts</h1>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Last Hangout</th>
+      <th>Location</th>
+      <th>Notes</th>
+    </tr>
+"""
